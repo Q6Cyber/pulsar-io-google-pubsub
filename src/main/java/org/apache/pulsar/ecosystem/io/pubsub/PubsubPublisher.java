8bd565f18ad76fbaf01d1950c6563f6d9798fd86
Jimmy Nguyen <jnguyen@q6cyber.com>
2024-03-05 11:29:02 -0500

    feat: update sink pubsub publisher to be synchronous

--- src/main/java/org/apache/pulsar/ecosystem/io/pubsub/PubsubPublisher.java
+++ src/main/java/org/apache/pulsar/ecosystem/io/pubsub/PubsubPublisher.java
@@ -223,6 +223,7 @@ public class PubsubPublisher {
         if (callback != null) {
             ApiFutures.addCallback(apiFuture, callback, MoreExecutors.directExecutor());
         }
+        apiFuture.get(30, TimeUnit.SECONDS);
     }
 
     private boolean hasSchema() {
